35a7640d9961dbb7292071716b7e8575ba9f90c4
Akhil Sharma <sharmakhil1704@gmail.com>
2025-07-12 10:25:25 +0530

    routes set

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,38 +1,29 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { BrowserRouter as Router, Routes, Route } from  'react-router-dom'
+import Navbar from './components/Navbar'
+import Login from './pages/Login'
+import Register from './pages/Register'
+import Home from './pages/Home'
+import ProductListing from './pages/ProductListing'
+import ProductDetail from './pages/ProductDetail'
+import Dashboard from './pages/Dashboard';
+import AdminPanel from './pages/AdminPanel';
+
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-    <div className="flex h-screen items-center justify-center bg-blue-500 text-white text-3xl font-bold">
-      Tailwind CSS Working 🎉
-    </div>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <Router>
+      <Navbar>
+        <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/products" element={<ProductListing />} />
+        <Route path="/products/:id" element={<ProductDetail />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/admin" element={<AdminPanel />} />       
+        </Routes>
+      </Navbar>
+    </Router>
+  );
 }
-
 export default App
